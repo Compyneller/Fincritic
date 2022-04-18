8265394e463620a55f8315100b72e1fb9b579b7c
Ronin <compyneller@gmail.com>
2022-04-18 19:42:43 +0530

    add ssl

--- src/Components/FooterBrand/FooterBrand.jsx
+++ src/Components/FooterBrand/FooterBrand.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../Assets/FINCRITIC_LOGO.png";
+import ssl from "../../Assets/ssl.png";
 
 const FooterBrand = ({ selectCountry }) => {
   return (
@@ -52,33 +53,44 @@ const FooterBrand = ({ selectCountry }) => {
       </div>
 
       <div className="underLine my-5"></div>
-      <div className="d-flex mb-4 footerUnderLink">
-        <Link
-          to="/risk"
-          onClick={() => {
-            window.scrollTo(0, 0);
-          }}
-        >
-          Disclosures and Risk Warning
-        </Link>
-        <Link
-          className="mx-5"
-          to="/privacy"
-          onClick={() => {
-            window.scrollTo(0, 0);
-          }}
-        >
-          Privacy Policy
-        </Link>
-        <Link
-          to="/customer"
-          onClick={() => {
-            window.scrollTo(0, 0);
-          }}
-        >
-          {" "}
-          Customer Policy
-        </Link>
+      <div className="row mb-5">
+        <div className="col-12 col-lg-9   ">
+          <div className="Disclosures d-flex align-items-center justify-content-between my-auto">
+            <Link
+              to="/risk"
+              onClick={() => {
+                window.scrollTo(0, 0);
+              }}
+            >
+              Disclosures and Risk Warning
+            </Link>
+            <Link
+              className="mx-5"
+              to="/privacy"
+              onClick={() => {
+                window.scrollTo(0, 0);
+              }}
+            >
+              Privacy Policy
+            </Link>
+            <Link
+              to="/customer"
+              onClick={() => {
+                window.scrollTo(0, 0);
+              }}
+            >
+              {" "}
+              Customer Policy
+            </Link>
+          </div>
+        </div>
+        <div className="col-12 col-lg-3 d-flex sslcontainer">
+          <img src={ssl} className="ssl" alt="" />{" "}
+          <p className="my-auto">
+            The Connection is protected by SSL certificate 256bit Secure
+            Processing{" "}
+          </p>
+        </div>
       </div>
       <p>
         Please note that foreign exchange trading and other leveraged trading
